4a91758daa02cd911a4a318fdccf66fd1cce6918
ZAHID188 <zahidpranta5@gmail.com>
2021-08-06 01:58:48 +0600

    Research added

--- docs/money_management/main.js
+++ docs/money_management/main.js
@@ -1,15 +1,13 @@
 new Vue({
   el: "#app",
   data: () => ({
-    valueTask: undefined,
+    result: undefined,
     tasksList: [
-      { name: 'Read more about Vue', done: false },
-      { name: 'Buy food', done: true },
-      { name: 'Learn AngularJS', done: false }
+      
     ],
+    tasksList2:[ 
 
-    firstNumber : '',
-    secondNumber: ''
+    ],
   }),
   computed: {
     taskClass() {
@@ -18,19 +16,107 @@ new Vue({
         'border-task': this.tasksList.length > 1
       }
     },
-    result: function(){
-      return this.firstNumber * this.secondNumber;
-  }
   },
   methods: {
     onSubmitForm() {
-      if (this.valueTask) {
-        this.tasksList.push({ name: this.valueTask, done: false })
-        this.valueTask = ''
+      if (this.money) {
+        var money=parseFloat(this.money)
+        var rate=parseFloat(this.rate)
+        var chance=parseFloat(this.chance)
+
+        var pr=money/this.ds(100,100)
+       // this.tasksList.push({ name: pr })
+
+        var invr=this.rtor(rate,pr)
+
+        var ex1=this.rtper(rate)
+        var ex2=this.ds(ex1,100); 
+        var s=parseFloat(0);
+        var res=parseFloat(0);
+        var temp2=parseFloat(0);
+        var  m=invr;
+        for(i=1;i<=chance;i++){
+          s=s+m;
+          m=(s+pr)/ex2;
+          res=s.toFixed(2)
+          this.tasksList.push({ name: res})
+          
+
+          temp2=temp2+s;
+
+        }
+
+       var  z=temp2.toFixed(2);
+        this.tasksList2.push({ name: z})
+        
+      
+        //this.money = ''
       }
     },
+
+//devide smaller number through bigger number-percent  (percent maker)       
+/*static double ds(double a, double b)
+{ 
+  double i, j;
+  i=a*100000;
+  j=(i/b)/100000;
+  return j;
+}
+*/
+    ds(x,y){ 
+      i1=parseFloat(x*100000)
+        j1=parseFloat((i1/y)/100000)
+      
+        
+        return j1
+      
+
+    },
+//rate to real investment in the first match
+/*
+static double rtor(double a, double b)
+{ 
+  double i, j,k,r;
+  i=a*1000;
+  j=i-1000;
+  k=1000/j;
+  r=k*b;
+  return r;
+}
+*/
+rtor(x,y){
+  i=parseFloat(x*1000);
+  j=parseFloat(i-1000);
+  k=parseFloat(1000/j);
+  r=parseFloat(k*y);
+  return r;
+},
+
+
+/* static double rtper(double a)
+ { 
+   double c, d, e, rtp;
+   c=a*10;
+   d=c-10;
+   e=d*10;
+   rtp=e;
+   return rtp;
+ } */
+
+ rtper(a){
+   c=parseFloat(a*10);
+   d=parseFloat(c-10);
+   e=parseFloat(d*10);
+   rtp=e;
+   return rtp;
+
+ },
+
+
+
     onRemoveTask(indexTask) {
       this.tasksList.splice(indexTask, 1)
-    }
+    },
+    
   }
 });
